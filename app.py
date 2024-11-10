91553059dd0993bc4d59ddd61b2440dfea390665
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-11-10 09:27:41 +0100

    App i testy

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(add(2, 3))         # Expected output: 5
+    print(multiply(2, 3))    # Expected output: 6
